1ceb276845a35ec171925b78961d36506e48cada
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-23 00:54:19 +0600

    sum of two matrix

--- array/matrixSum.java
+++ array/matrixSum.java
@@ -6,36 +6,54 @@ public class matrixSum {
         Scanner s = new Scanner(System.in);
         int [][] A = new int[2][3];
         int [][] B = new int[2][3];
+        int [][] R = new int[2][3];
 
-        System.out.println("Enter the element of A matrix: ");
-        for(int row = 0; row<2; row++){
-            for(int col = 0; col<3; col++){
-                System.out.printf("A[%d][%d] = ",row,col);
+
+        System.out.println("Enter the elements of A matrix: ");
+        for(int row = 0; row < 2; row++){
+            for(int col = 0; col < 3; col++){
+                System.out.printf("A[%d][%d] = ", row, col);
                 A[row][col] = s.nextInt();
             }
         }
 
-        System.out.println("Enter the element of B matrix: ");
-        for(int row = 0; row<2; row++){
-            for(int col = 0; col<3; col++){
-                System.out.printf("A[%d][%d] = ",row,col);
-                A[row][col] = s.nextInt();
+
+        System.out.println("Enter the elements of B matrix: ");
+        for(int row = 0; row < 2; row++){
+            for(int col = 0; col < 3; col++){
+                System.out.printf("B[%d][%d] = ", row, col);
+                B[row][col] = s.nextInt(); 
             }
         }
+
+
         System.out.println("Entered A matrix: ");
-        for(int row = 0; row<2; row++){
-            for(int col = 0; col<3; col++){
-                System.out.print(A[row][col]+" ");
+        for(int row = 0; row < 2; row++){
+            for(int col = 0; col < 3; col++){
+                System.out.print(A[row][col] + " ");
             }
             System.out.println();
         }
+
+
         System.out.println("Entered B matrix: ");
-        for(int row = 0; row<2; row++){
-            for(int col = 0; col<3; col++){
-                System.out.print(A[row][col]+" ");
+        for(int row = 0; row < 2; row++){
+            for(int col = 0; col < 3; col++){
+                System.out.print(B[row][col] + " "); 
             }
             System.out.println();
         }
+
+        System.out.println("Sum of matrix A and B: ");
+        for(int row = 0; row < 2; row++){
+            for(int col = 0; col < 3; col++){
+                //R[row][col] = A[row][col] + B[row][col];  
+                //System.out.print(" " + R[row][col] );  
+                System.out.print(" " + (A[row][col] + B[row][col]) );  
+            }
+            System.out.println();
+        }
+
         s.close();
     }
 }
