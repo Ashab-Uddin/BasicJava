83a10e36d2922628515c76805fb68f1cdade91e9
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-23 00:32:42 +0600

    matrix sum

--- array/matrixSum.java
+++ array/matrixSum.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 
 public class matrixSum {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);  
-        int[][] mats1 = matrixUtility.input2Darray1(s);  
+        Scanner s = new Scanner(System.in);
+        int [][] A = new int[2][3];
+        int [][] B = new int[2][3];
 
-        System.out.println("Your Entered 1st Matrix:");
-        for (int i = 0; i < mats1.length; i++) {
-            for (int j = 0; j < mats1[i].length; j++) {
-                System.out.print(mats1[i][j] + " ");
+        System.out.println("Enter the element of A matrix: ");
+        for(int row = 0; row<2; row++){
+            for(int col = 0; col<3; col++){
+                A[row][col] = s.nextInt();
+            }
+        }
+        System.out.println("Entered A matrix: ");
+        for(int row = 0; row<2; row++){
+            for(int col = 0; col<3; col++){
+                System.out.print(A[row][col]+" ");
             }
             System.out.println();
         }
-
-        s.close(); 
+        s.close();
     }
 }
--- array/matrixUtility.java
+++ array/matrixUtility.java
@@ -2,23 +2,34 @@ package array;
 import java.util.Scanner;
 
 public class matrixUtility {
-    public static  int[][] input2Darray1(Scanner s) {
+    public static int[][][] input2DArrays(Scanner s) {
+        // Input 1st matrix
         System.out.print("Enter the row of 1st matrix: ");
-        int row = s.nextInt();
-
+        int row1 = s.nextInt();
         System.out.print("Enter the col of 1st matrix: ");
-        int col = s.nextInt();
-
-        int[][]matArray1 = new int[row][col];
-
-        for(int i = 0;i<row;i++){
-            for(int j = 0; j<col; j++){
+        int col1 = s.nextInt();
+        int[][] matArray1 = new int[row1][col1];
+        for (int i = 0; i < row1; i++) {
+            for (int j = 0; j < col1; j++) {
                 System.out.print("Enter element at [" + i + "][" + j + "]: ");
                 matArray1[i][j] = s.nextInt();
             }
         }
-        s.close();
-        return matArray1;
 
+        // Input 2nd matrix
+        System.out.print("Enter the row of 2nd matrix: ");
+        int row2 = s.nextInt();
+        System.out.print("Enter the col of 2nd matrix: ");
+        int col2 = s.nextInt();
+        int[][] matArray2 = new int[row2][col2];
+        for (int i = 0; i < row2; i++) {
+            for (int j = 0; j < col2; j++) {
+                System.out.print("Enter element at [" + i + "][" + j + "]: ");
+                matArray2[i][j] = s.nextInt();
+            }
+        }
+
+        // Return both matrices as a 3D array
+        return new int[][][]{matArray1, matArray2};
     }
 }
