e46d907bfb6796754baf0b0a333efe5583581ed1
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-23 01:50:24 +0600

    complete the problem sum of the lower triangle matrix

--- array/lowerTriangle.java
+++ array/lowerTriangle.java
@@ -4,6 +4,7 @@ public class lowerTriangle {
     public static void main(String[] args) {
         int [][]lowerTriangleM = matrixUtility.inputMatrix();
         int n = lowerTriangleM.length;
+        int sumOfLowerTriangleM = 0;
 
         for(int row = 0; row<n; row++){
             for(int col = 0; col<n; col++){
@@ -11,5 +12,15 @@ public class lowerTriangle {
             }
             System.out.println();
         }
+
+        for(int row = 0; row<n; row++){
+            for(int col = 0; col<n; col++){
+                if(row>col){
+                    sumOfLowerTriangleM += lowerTriangleM[row][col];
+                }
+            }
+        }
+
+        System.out.println("Sum of the lower triangle matrix: "+sumOfLowerTriangleM);
     }
 }
