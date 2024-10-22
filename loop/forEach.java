14dcd66274734e3c1a86df6a8a09f4eca02f35ff
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-22 22:19:14 +0600

    Explore the For Each loop

---
+++ loop/forEach.java
@@ -0,0 +1,10 @@
+package loop;
+
+public class forEach {
+    public static void main(String[] args) {
+        String[] names = {"ashab","uddin","ibrahim","modo","topu"};
+        for(String x:names){
+            System.out.println(x);
+        }
+    }
+}
