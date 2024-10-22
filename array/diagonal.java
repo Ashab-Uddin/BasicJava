a50c1567f9c0bc6ce7b1d748ec92dc1e238e5faa
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-23 01:23:31 +0600

    find sum of diagonal of A matrix

--- array/diagonal.java
+++ array/diagonal.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class diagonal {
 public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-    int [][] A = new int[2][3];
+    int [][] A = new int[3][3];
+    int sumOfDiagonal = 0;
 
     System.out.println("Enter the elements of A matrix: ");
-        for(int row = 0; row < 2; row++){
+        for(int row = 0; row < 3; row++){
             for(int col = 0; col < 3; col++){
                 System.out.printf("A[%d][%d] = ", row, col);
                 A[row][col] = s.nextInt();
@@ -17,12 +18,25 @@ public static void main(String[] args) {
 
 
         System.out.println("Entered A matrix: ");
-        for(int row = 0; row < 2; row++){
+        for(int row = 0; row < 3; row++){
             for(int col = 0; col < 3; col++){
                 System.out.print(A[row][col] + " ");
             }
             System.out.println();
         }
+
+        for(int row = 0; row<3; row++){
+            for(int col = 0; col<3; col++){
+                if(row == col){
+                    sumOfDiagonal += A[row][col];
+                }
+            }
+        }
+
+        System.out.println("Sum of the Diagonal of A matrix: "+sumOfDiagonal);
+
+
+
         s.close();
 
 }
