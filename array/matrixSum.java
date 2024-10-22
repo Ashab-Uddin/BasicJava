3ea9bd64f382077d5b3adbaf1ba050028d0e5f1f
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-23 00:36:49 +0600

    matrix summation

--- array/matrixSum.java
+++ array/matrixSum.java
@@ -10,10 +10,26 @@ public class matrixSum {
         System.out.println("Enter the element of A matrix: ");
         for(int row = 0; row<2; row++){
             for(int col = 0; col<3; col++){
+                System.out.printf("A[%d][%d] = ",row,col);
+                A[row][col] = s.nextInt();
+            }
+        }
+
+        System.out.println("Enter the element of B matrix: ");
+        for(int row = 0; row<2; row++){
+            for(int col = 0; col<3; col++){
+                System.out.printf("A[%d][%d] = ",row,col);
                 A[row][col] = s.nextInt();
             }
         }
         System.out.println("Entered A matrix: ");
+        for(int row = 0; row<2; row++){
+            for(int col = 0; col<3; col++){
+                System.out.print(A[row][col]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("Entered B matrix: ");
         for(int row = 0; row<2; row++){
             for(int col = 0; col<3; col++){
                 System.out.print(A[row][col]+" ");
