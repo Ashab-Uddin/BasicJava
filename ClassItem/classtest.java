ce985ccff3448e06ca96ca349afacc43f8bf2fd7
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-23 14:50:42 +0600

    Create a class to learn basic concept and create Method

---
+++ ClassItem/classtest.java
@@ -0,0 +1,28 @@
+package ClassItem;
+
+public class classtest {
+    public static void main(String[] args) {
+        students student1 = new students();
+        student1.name ="Ashab uddin";
+        student1.dept = "CSE";
+        student1.batch = "242";
+        student1.program = "Undergaduate";
+        student1.id = 232002274;
+        student1.displayinfo();
+    }
+}
+
+
+class students {  
+    String name, dept , batch, program;
+    int id;
+
+    void displayinfo(){
+        System.out.println("Student Information: ");
+        System.out.println("Name: "+name);
+        System.out.println("Dept: "+dept);
+        System.out.println("Batch: "+batch);
+        System.out.println("Program: "+program);
+        System.out.println("ID: "+id);
+    }
+}
