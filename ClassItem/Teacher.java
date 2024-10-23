a89b05c54427de8efdba366dc3700649cb00041b
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-23 14:38:01 +0600

    Create method

--- ClassItem/Teacher.java
+++ ClassItem/Teacher.java
@@ -4,4 +4,10 @@ package ClassItem;
 public class Teacher { 
     String name, dept;
     int id;
+
+    void displayinfo(){
+        System.out.println("Name: "+name);
+        System.out.println("Dept: "+dept);
+        System.out.println("ID: "+id);
+    }
 }
--- ClassItem/test.java
+++ ClassItem/test.java
@@ -1,15 +1,24 @@
 package ClassItem;
 
+
 public class test {
     public static void main(String[] args) {
-        Teacher teacher1 = new Teacher(); 
+        Teacher teacher1 = new Teacher();
+        Teacher teacher2 = new Teacher();
+
         teacher1.name = "Ashab";
         teacher1.dept = "CSE";
         teacher1.id = 202;
 
+        teacher2.name = "Imran";
+        teacher2.dept = "EEE";
+        teacher2.id = 201;
 
-        System.out.println("Teacher's Name: " + teacher1.name);
-        System.out.println("Teacher's Department: " + teacher1.dept);
-        System.out.println("Teacher's ID: " + teacher1.id);
+
+        System.out.println("Teacher's Name: " + teacher2.name);
+        System.out.println("Teacher's Department: " + teacher2.dept);
+        System.out.println("Teacher's ID: " + teacher2.id);
+        System.out.println();
+        teacher1.displayinfo();
     }
 }
