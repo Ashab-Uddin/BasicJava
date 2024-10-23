906f698f06ea53943c9def9d7030bd426e88d570
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-23 17:26:50 +0600

    Create constractor Method and write a program using constractor

---
+++ Constractor/basicConstractor.java
@@ -0,0 +1,29 @@
+package Constractor;
+class StudentInfo{
+    String name,dept,program;
+    int id,batch;
+    StudentInfo(String n, String d, String p, int i, int b){
+        name = n;
+        dept = d;
+        program = p;
+        id = i;
+        batch = b;
+
+    }
+    void displayinfo(){
+        System.out.println("Student Information");
+        System.out.println("Name: "+name);
+        System.out.println("Department: "+dept);
+        System.out.println("Program: "+program);
+        System.out.println("Student ID: "+id);
+        System.out.println("Batch: "+batch);
+    }
+}
+
+public class basicConstractor {
+    public static void main(String[] args) {
+        StudentInfo student1 = new StudentInfo("Ashab Uddin","CSE","Undergraduate",232002274,232);
+        student1.displayinfo();
+
+    }
+}
