c90e76a3d225bd41f0c3e0bf82aa6178f851195b
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-24 02:24:00 +0600

    Explore Call by Refernce

---
+++ callByValue_Reference/callByReferenceTest.java
@@ -0,0 +1,19 @@
+package callByValue_Reference;
+class callByReference{
+    String name;
+    void change(callByReference r2){
+        r2.name ="Ashab";
+    }
+
+}
+
+public class callByReferenceTest {
+    public static void main(String[] args) {
+        callByReference r1 = new callByReference();
+        r1.name = "Sohel";
+        System.out.println("Before Calling: "+r1.name);
+
+        r1.change(r1);
+        System.out.println("After Calling: "+r1.name);
+    }
+}
