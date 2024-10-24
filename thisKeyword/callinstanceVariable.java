49696b693d59f7f265c44e5e1672cd6488ccbe02
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-24 18:23:48 +0600

    Explore the this keyword with proper example

--- thisKeyword/callinstanceVariable.java
+++ thisKeyword/callinstanceVariable.java
@@ -1,24 +1,33 @@
 package thisKeyword;
 class person{
-    String name;
+    String name,hairColor;
     int age;
+    
 
     person(String name,int age){
         this.name = name;
         this.age = age;
 
     }
+    person(String name ,int age, String hairColor){
+        this(name,age);
+        this.hairColor = hairColor;
+    }
     
     void displayPerson(){
         System.out.println("Person Information");
         System.out.println("Name: "+name);
         System.out.println("Age: "+age);
+        System.out.println("Hair Color: "+hairColor);
     }
 }
 
 public class callinstanceVariable {
     public static void main(String[] args) {
-        person p = new person("Ashab Uddin",  22);
-        p.displayPerson();
+        person p1 = new person("Ashab Uddin",  22);
+        p1.displayPerson();
+
+        person p2 = new person("Imran", 24,"red");
+        p2.displayPerson();
     }
 }
