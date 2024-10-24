536ef6bb7df38cef15fb66d8dcd81721d38587b9
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-24 19:20:17 +0600

    Explore the Polymorhism

---
+++ Polymorhism/polyTest.java
@@ -0,0 +1,29 @@
+package Polymorhism;
+class Person{
+    void display(){
+        System.out.println("I am person");
+    }
+}
+class Teacher extends Person{
+    void display(){
+        System.out.println("I am Teacher");
+    }
+}
+class Student extends Person{
+    void display(){
+        System.out.println("I am student");
+    }
+}
+
+public class polyTest {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.display();
+
+        Teacher t = new Teacher();
+        t.display();
+
+        Student s = new Student();
+        s.display();
+    }
+}
