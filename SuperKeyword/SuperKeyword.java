cf2bd051bd911ed546b90098bb66d4651cc1e823
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-24 17:01:07 +0600

    Explore the super class and print

--- SuperKeyword/SuperKeyword.java
+++ SuperKeyword/SuperKeyword.java
@@ -1,12 +1,14 @@
 package SuperKeyword;
 class A{
+    //it is super class
     int x = 5;
 }
 class B extends A{
     int x = 10;
     
     void display(){
-        System.out.println(x);
+        System.out.println("main class x = "+x);
+        System.out.println("super class x = "+super.x);
     }
 
 }
