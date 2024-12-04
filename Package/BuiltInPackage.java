c6be648ee880fa73f9961913af2a798dc35a223e
Ashab Uddin <ashabuddin47@gmail.com>
2024-12-04 08:17:49 +0600

    I learn Built In Package

---
+++ Package/BuiltInPackage.java
@@ -0,0 +1,10 @@
+package Package;
+import java.util.Scanner;
+public class BuiltInPackage {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        s.close();
+    }
+    
+}
