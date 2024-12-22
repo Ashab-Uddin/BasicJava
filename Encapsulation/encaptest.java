c2a460717f4b3c6b8e230fb5029f11e8edc1b27f
Ashab Uddin <ashabuddin47@gmail.com>
2024-12-22 13:13:57 +0600

    encapsulation

---
+++ Encapsulation/encaptest.java
@@ -0,0 +1,12 @@
+package Encapsulation;
+
+public class encaptest {
+    public static void main(String[] args) {
+        encap enObj = new encap();
+        enObj.setName("Ashab Uddin");
+        enObj.setId(232);
+        System.out.println("Name: "+enObj.getName());
+        System.out.println("ID: "+enObj.getId());
+
+    }
+}
